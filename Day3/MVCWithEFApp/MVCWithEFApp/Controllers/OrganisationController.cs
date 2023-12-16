58c4fca5a2c3794d77b7f728cdd2d01394818bdd
kannan <kannan.mumbai@yahoo.com>
2023-12-16 16:49:57 +0530

    add

--- Day3/MVCWithEFApp/MVCWithEFApp/Controllers/OrganisationController.cs
+++ Day3/MVCWithEFApp/MVCWithEFApp/Controllers/OrganisationController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCWithEFApp.Domain;
 using MVCWithEFApp.Repository;
 
 namespace MVCWithEFApp.Controllers
@@ -6,12 +7,46 @@ namespace MVCWithEFApp.Controllers
     public class OrganisationController : Controller
     {
         private readonly IOrganizationRepostiory _orgnizationRepo;
-        public OrganisationController(IOrganizationRepostiory orgRepo) { 
-          _orgnizationRepo = orgRepo;
+        public OrganisationController(IOrganizationRepostiory orgRepo)
+        {
+            _orgnizationRepo = orgRepo;
         }
         public IActionResult Index()
         {
-             return View(_orgnizationRepo.GetOrganizations());
+            return View(_orgnizationRepo.GetOrganizations());
+        }
+
+        public IActionResult Add()
+        {
+            Organization vm = new Organization();
+            vm.Id = 0;
+            vm.Name = string.Empty;
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Organization vm)
+        {
+
+            _orgnizationRepo.AddOrganization(vm);
+
+            return  RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id) {
+
+          var vm=  _orgnizationRepo.GetOrganizationById(id);
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Organization vm)
+        {
+            _orgnizationRepo.UpdateOrganization(vm);
+
+            return RedirectToAction("Index");
+          
         }
     }
 }
