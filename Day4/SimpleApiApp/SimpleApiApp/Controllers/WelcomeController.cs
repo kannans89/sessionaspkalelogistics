27f3b54e68629bb568fb064ab658fae2a8f1dc1d
kannan <kannan.mumbai@yahoo.com>
2023-12-17 17:00:14 +0530

    'api'

---
+++ Day4/SimpleApiApp/SimpleApiApp/Controllers/WelcomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleApiApp.Controllers
+{
+	[ApiController]
+	[Route("api/v1/[controller]")]
+	public class WelcomeController:ControllerBase
+	{
+
+		[HttpGet]
+		public string Get() {
+			return "Get called";
+		}
+
+		[HttpPost]
+		public string Post() {
+			return "Post called";
+		}
+
+		[HttpPut]
+		public string Put()
+		{
+			return "Put called";
+		}
+
+		[HttpDelete]
+		public string Delete()
+		{
+			return "Delete called";
+		}
+
+	}
+}
