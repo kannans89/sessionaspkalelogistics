9a939d0f6aed9ef13a6e6c9de9cafff6a01133a1
kannan <kannan.mumbai@yahoo.com>
2023-12-17 15:46:59 +0530

    exceptions

---
+++ Day4/ExceptionsAndLoggingApp/ExceptionsAndLoggingApp/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using ExceptionsAndLoggingApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ExceptionsAndLoggingApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+			_logger.LogInformation("inside home controller".ToUpper());
+			Console.WriteLine(logger.GetType().ToString().ToUpper()) ;
+        }
+
+		public IActionResult Index()
+		{
+			_logger.LogCritical("inside home controller".ToUpper());
+			_logger.LogError("inside home controller".ToUpper());
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			_logger.LogWarning("inside home controller".ToUpper());
+
+			throw new Exception("Invalid operation");
+			//return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
