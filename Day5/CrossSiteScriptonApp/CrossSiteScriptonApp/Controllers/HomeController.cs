4e81c611738c160d545f7e9c215946cec6038594
kannan <kannan.mumbai@yahoo.com>
2023-12-23 10:57:34 +0530

    save

---
+++ Day5/CrossSiteScriptonApp/CrossSiteScriptonApp/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using CrossSiteScriptonApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CrossSiteScriptonApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult SendComment()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendComment(string comment)
+        {
+
+            ViewBag.Comment = comment;
+            return View();
+        }
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
